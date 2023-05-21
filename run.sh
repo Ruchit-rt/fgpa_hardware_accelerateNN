5277cb98d1ef66ae208214c1c527e7e360a6d0da
abdur2001 <rahim@shariftextiles.com>
2023-05-21 15:41:43 -0700

    Add dpc++ model from urop

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source /opt/intel/oneapi/setvars.sh > /dev/null 2>&1
 /bin/echo "##" $(whoami) is compiling DPCPP_Essentials Module2 -- SYCL Program Structure sample - 1 of 7 gpu_sample.cpp
-icpx -fsycl src/model_encoder.cpp
+icpx -fsycl -std=c++17 src/model_urop.cpp
 if [ $? -eq 0 ]; then ./a.out; fi
