d1b4751d66d1bf3a9f35d70e0dfe3af3ebf8c057
abdur2001 <rahim@shariftextiles.com>
2023-05-24 08:48:54 -0700

    Add scripts for running executables

--- scripts/hardware.sh
+++ scripts/hardware.sh
@@ -1,2 +1,3 @@
-# FPGA hardware image:
+#!/bin/bash
+source /opt/intel/oneapi/setvars.sh > /dev/null 2>&1
 icpx -fsycl -fintelfpga -DFPGA_HARDWARE src/model_quantised.cpp -Xshardware -Xstarget=Stratix10 -o model_quantised.fpga
--- scripts/simulate.sh
+++ scripts/simulate.sh
@@ -1,3 +1,3 @@
+#!/bin/bash
 source /opt/intel/oneapi/setvars.sh > /dev/null 2>&1
 icpx -fsycl -fintelfpga -DFPGA_SIMULATOR src/model_quantised.cpp -Xssimulation -Xstarget=Stratix10 -Xsghdl -o model_quantised.a
-if [ $? -eq 0 ]; then CL_CONTEXT_MPSIM_DEVICE_INTELFPGA=1 ./model_quantised.fpga_sim; fi
